dddb199d341ca5db30f6d3c6fe3bb2f8549d3d6f
Logan Armstrong <ffvx43@durham.ac.uk>
2020-10-21 17:42:35 +0100

    Add slightly improved menu

--- revisionTool.py
+++ revisionTool.py
@@ -1,13 +1,29 @@
 from colorama import init, Fore, Back
 import os
+import msvcrt as m
+import sys
+from time import sleep
+
+#methods
+def wait():
+    m.getch()
+def typewrite(string):
+    for char in string:
+        sleep(0.01)
+        sys.stderr.write(char)
+#setup    
 init()
 clear = lambda: os.system('cls')
 isRunning = True
 print(Fore.GREEN)
+
+#main
 while (isRunning):
-    print("Select one of the options below [1-7]\n 1. Start a revision session\n 2. Add a module\n 3. Add a subtopic\n 4. View daily report\n 5. View weekly report\n 6. Add break suggestions\n 7. Exit")
+    
+    typewrite("Select one of the options below [1-7]\n 1. Start a revision session\n 2. Add a module\n 3. Add a subtopic\n 4. View daily report\n 5. View weekly report\n 6. Add break suggestions\n 7. Exit\n")
     choice = input()
-    if (choice == "1"):
+    clear()   
+    if (choice == "1"):        
         print("You chose 1")
     elif (choice == "2"):
         print("You chose 2")
@@ -21,5 +37,8 @@ while (isRunning):
         print("You chose 6")
     elif (choice == "7"):
         isRunning = False
+        break
     else:
         print("Invalid option, please enter value 1-7")
+    wait()
+    clear()
