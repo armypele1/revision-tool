157126b76ad7aeab377146e78df3800f7e096cdc
Logan Armstrong <ffvx43@durham.ac.uk>
2020-10-21 17:11:37 +0100

    Basic menu system[C

--- revisionTool.py
+++ revisionTool.py
@@ -1,4 +1,25 @@
-from colorama import init, Fore
+from colorama import init, Fore, Back
+import os
 init()
-
-print(Fore.GREEN + 'Hello World')
+clear = lambda: os.system('cls')
+isRunning = True
+print(Fore.GREEN)
+while (isRunning):
+    print("Select one of the options below [1-7]\n 1. Start a revision session\n 2. Add a module\n 3. Add a subtopic\n 4. View daily report\n 5. View weekly report\n 6. Add break suggestions\n 7. Exit")
+    choice = input()
+    if (choice == "1"):
+        print("You chose 1")
+    elif (choice == "2"):
+        print("You chose 2")
+    elif (choice == "3"):
+        print("You chose 3")
+    elif (choice == "4"):
+        print("You chose 4")
+    elif (choice == "5"):
+        print("You chose 5")
+    elif (choice == "6"):
+        print("You chose 6")
+    elif (choice == "7"):
+        isRunning = False
+    else:
+        print("Invalid option, please enter value 1-7")
